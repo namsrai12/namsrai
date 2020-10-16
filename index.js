c3871465aedd4fa7e5eb97a4ab843a747538bc15
namsrai12 <72962686+namsrai12@users.noreply.github.com>
2020-10-16 14:58:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function mendchilgee(){
+function Mendchilgee(){
 return <h1>hello</h1>;
-
 };
-function mendchilgee2(props){
+function Mendchilgee2(props){
 return <h1>{props.ner}</h1>;
-}
+};
 
-function mendchilgee3(props){
+function Mendchilgee3(props){
     return(<div>
         <h1>{props.ner}</h1>
     <p>{props.nas}</p>
-</div>        )}
+</div>        )};
 function EtsegComp(props){
     return(
         <div>
-            <mendchilgee/>
-            <mendchilgee2 ner={props.ner}/>
-            <mendchilgee3/>
+            <Mendchilgee/>
+            <Mendchilgee2 ner={props.ner}/>
+            <Mendchilgee3/>
             <table>
     <tr><td>{props.angi[1]}</td></tr>
     <tr><td>{props.angi[2]}</td></tr>
@@ -26,7 +25,7 @@ function EtsegComp(props){
     )
 }
 ReactDOM.render(
-<EtsegComp ner="james" nas="25" angi={['программ хангамж','MC','нябо-мэдээллийн систэм']}/>
+<EtsegComp ner="james" nas="25" angi={['программ хангамж','MC','нябо-мэдээллийн систэм']}/>,
 document.getElementById('root')
 
 
